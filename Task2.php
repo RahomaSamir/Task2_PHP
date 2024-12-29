9baac675942e593f1e87ab0c72a2805dcc8f11f1
RahmaSamir <you@example.com>
2024-12-29 12:44:26 +0200

    Rahma Samir

---
+++ Task2.php
@@ -0,0 +1,75 @@
+<?php
+ //Search for how to make \n work in browser
+/*
+$text = "PHP is \n an open-source \n server-side scripting language.  ";
+echo nl2br($text);
+
+//Display $_SERVER in readable format.
+echo"<pre>";
+print_r($_SERVER);
+echo "</pre>";
+
+//Try any three functions from String or Arrays built in.
+// Join 
+$arr = array('Hello', 'everybody', 'in' , 'PHP' , 'Page');
+echo join( " ", $arr);
+
+// arr function splice
+$a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$a2=array("a"=>"purple","b"=>"orange");
+array_splice($a1,0,2,$a2);
+print_r($a1);
+
+//split
+print_r(str_split("Hello"));
+
+//4-  Write a PHP script to get the sum and avg of an indexed array
+// with value = 45 in index =1
+// with value = 12 in index =0
+// with value = 25 in index =3
+// with value = 10 in index =2
+ 
+// after that sort it in a reverse order (highest to lowest).
+
+$arr = array(12,45,10,25);
+ $sum = array_sum($arr);
+ $avg = $sum / 4;
+echo "The sum of array = $sum ";
+echo "<br>";
+echo "The average of arrar = $avg";
+echo "<br>";
+rsort($arr);
+print_r($arr);
+
+
+// 5-  Write a PHP script to sort the following associative array :
+ 
+// array("Sara"=>31,"John"=>41,"Walaa"=>39,"Ramy"=>40) in
+
+$arr1 = array("Sara"=>31,"John"=>41,"Walaa"=>39,"Ramy"=>40);
+// a) ascending order sort by value
+asort($arr1);
+echo " Ascending order sort by value: ";
+echo"<br>";
+print_r($arr1);
+echo"<br>";
+// b) ascending order sort by Key
+ksort($arr1);
+echo " Ascending order sort by key: ";
+echo"<br>";
+print_r($arr1);
+echo"<br>";
+// c) descending order sorting by Value
+arsort($arr1);
+echo " Descending order sorting by Value: ";
+echo"<br>";
+print_r($arr1);
+echo "<br>";
+// d) descending order sorting by Key
+krsort($arr1);
+echo " Descending order sorting by Key: ";
+echo"<br>";
+print_r($arr1);
+echo "<br>";
+*/
+?>
